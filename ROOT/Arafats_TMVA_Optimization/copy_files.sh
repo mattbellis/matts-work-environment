837364cc6474c213f8a0f6047959118097fe487b
Matt Bellis <matthew.bellis@gmail.com>
2011-06-13 12:21:01 -0700

    Added Arafats optimization code.

---
+++ ROOT/Arafats_TMVA_Optimization/copy_files.sh
@@ -0,0 +1,2 @@
+scp *.root yakut16.slac.stanford.edu:~/LeptBc_AWG120_bellis/data/Arafats_TMVA_Optimization/.
+scp "yakut16.slac.stanford.edu:~/LeptBc_AWG120_bellis/data/Arafats_TMVA_Optimization/*.root" .
