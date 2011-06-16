eb42e1e272bc192c0fbb8508feb005bbb0d4c16a
Matthew Bellis <matthew.bellis@gmail.com>
2011-06-16 01:14:49 -0700

    Some small changes.

--- ROOT/Arafats_TMVA_Optimization/copy_files.sh
+++ ROOT/Arafats_TMVA_Optimization/copy_files.sh
@@ -1,2 +1,2 @@
-scp data/*.root yakut16.slac.stanford.edu:~/LeptBc_AWG120_bellis/data/Arafats_TMVA_Optimization/.
-#scp "yakut16.slac.stanford.edu:~/LeptBc_AWG120_bellis/data/Arafats_TMVA_Optimization/*.root" data/.
+#scp data/*.root yakut16.slac.stanford.edu:~/LeptBc_AWG120_bellis/data/Arafats_TMVA_Optimization/.
+scp "yakut16.slac.stanford.edu:~/LeptBc_AWG120_bellis/data/Arafats_TMVA_Optimization/*.root" data/.
