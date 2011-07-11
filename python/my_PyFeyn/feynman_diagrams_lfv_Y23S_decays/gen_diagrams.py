c58e584c8047b2ba9356ae7410f2bb7086baad31
Matthew Bellis <matthew.bellis@gmail.com>
2011-07-10 22:58:16 -0700

    Diagrams working!

--- python/my_PyFeyn/feynman_diagrams_lfv_Y23S_decays/gen_diagrams.py
+++ python/my_PyFeyn/feynman_diagrams_lfv_Y23S_decays/gen_diagrams.py
@@ -3,11 +3,11 @@
 from diagrams import *
 tag = "_blk_bkg"
 
-'''
+#'''
 for i in range(0,7):
     name = "bbar_ss_loop_%s_%d" % (tag,i)
     bbbar_ss_loop(name,i)
-'''
+#'''
 
 for i in range(0,2):
     name = "bbar_leptoq_4pt_%s_%d" % (tag,i)
