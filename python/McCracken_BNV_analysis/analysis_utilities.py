aaa19ebf687e182c46d6b9d063533e22734b123e
Matthew Bellis <matthew.bellis@gmail.com>
2012-04-17 22:57:09 -0500

    Modded code with some numpy stuff.

--- python/McCracken_BNV_analysis/analysis_utilities.py
+++ python/McCracken_BNV_analysis/analysis_utilities.py
@@ -10,7 +10,7 @@ import numpy as np
 ################################################################################
 def magnitude_of_3vec(v3):
 
-    magnitude = sqrt(v3[0]*v3[0] + v3[1]*v3[1] + v3[2]*v3[2])
+    magnitude = np.sqrt(v3[0]*v3[0] + v3[1]*v3[1] + v3[2]*v3[2])
 
     return magnitude
 
@@ -35,9 +35,20 @@ def mass_from_special_relativity(v4):
     mass_squared = v4[0]*v4[0] - pmag*pmag
 
     if mass_squared>0:
-        return sqrt(mass_squared)
+        return np.sqrt(mass_squared)
     else:
-        return -sqrt(abs(mass_squared))
+        return -np.sqrt(abs(mass_squared))
+
+################################################################################
+# Calculate the mass of a particle using Special Relativity
+################################################################################
+def mass2_from_special_relativity(v4):
+
+    pmag2 = v4[1]**2 + v4[2]**2 + v4[3]**2
+
+    mass_squared = v4[0]**2 - pmag2
+
+    return mass_squared
 
 ################################################################################
 
@@ -46,5 +57,6 @@ mass_pi = 0.139570
 mass_k = 0.493677
 mass_p = 0.938272
 
+target = np.array([mass_p,0.0,0.0,0.0])
 target = np.array([mass_p,0.0,0.0,0.0])
 
