2b2b3fdc826e8159f72ff5d8d90641cb74dffc1a
Matt Bellis <matthew.bellis@gmail.com>
2012-05-26 22:23:39 -0500

    Better plotting for BNV/Lambda

--- python/McCracken_BNV_analysis/read_in_calculated_data_and_plot.py
+++ python/McCracken_BNV_analysis/read_in_calculated_data_and_plot.py
@@ -17,12 +17,20 @@ import lichen.lichen as lch
 infile = open(sys.argv[1],'r')
 
 n = 0
-masses = [[],[],[]]
-vtxs = [[],[]]
+masses = [np.array([]),np.array([]),np.array([])]
+bad_masses = [np.array([]),np.array([]),np.array([])]
+vtxs = [np.array([]),np.array([])]
 flight_length = []
 count = 0
 beam = None
 nvtx = 0
+
+max_events = 1000
+
+mtitles = []
+mtitles.append(r"Invariant mass of the $\Lambda$ candidate")
+mtitles.append(r'Total missing mass squared')
+mtitles.append(r"Missing mass off the $K^{+}$ candidate")
 
 for line in infile:
     #vals = line.split()
@@ -36,33 +44,36 @@ for line in infile:
 
     if no_nans:
 
-        if abs(vals[0])<abs(vals[3]):
-            masses[0].append(vals[0])
-            #vtxs[0].append(vals[6])
-            #vtxs[1].append(vals[7])
+        if abs(vals[0]-1.115)<abs(vals[3]-1.115):
+            masses[0] = np.append(masses[0],vals[0])
+            bad_masses[0] = np.append(bad_masses[0],vals[3])
         else:
-            masses[0].append(vals[3])
-            #vtxs[0].append(vals[7])
-            #vtxs[1].append(vals[6])
+            masses[0] = np.append(masses[0],vals[3])
+            bad_masses[0] = np.append(bad_masses[0],vals[0])
 
-        if abs(vals[1]-1.115)<abs(vals[4]-1.115):
-            masses[1].append(vals[1])
+        if abs(vals[1])<abs(vals[4]):
+            masses[1] = np.append(masses[1],vals[1])
+            bad_masses[1] = np.append(bad_masses[1],vals[4])
         else:
-            masses[1].append(vals[4])
+            masses[1] = np.append(masses[1],vals[4])
+            bad_masses[1] = np.append(bad_masses[1],vals[1])
 
         if abs(vals[2]-1.115)<abs(vals[5]-1.115):
-            masses[2].append(vals[2])
+            masses[2] = np.append(masses[2],vals[2])
+            bad_masses[2] = np.append(bad_masses[2],vals[5])
         else:
-            masses[2].append(vals[5])
+            masses[2] = np.append(masses[2],vals[5])
+            bad_masses[2] = np.append(bad_masses[2],vals[2])
 
     count +=1 
-    if count > 10000:
+    if count>max_events:
         break
     if count%10000==0:
         print count
 
 
-nfigs = 2
+print "\n"
+nfigs = 3
 figs = []
 for i in xrange(nfigs):
     name = "fig%d" % (i)
@@ -78,28 +89,53 @@ for i in xrange(nfigs):
 
     # Adjust the spacing between the subplots, to allow for better
     # readability.
-    figs[i].subplots_adjust(wspace=0.4,hspace=0.6,bottom=0.2)
+    #figs[i].subplots_adjust(wspace=0.4,hspace=0.6,bottom=0.2)
+    figs[i].subplots_adjust(left=0.07, bottom=0.15, right=0.95, wspace=0.30, hspace=None)
 
 
 
 #print masses
+print 'Masses'
 print len(masses[0])
 print len(masses[1])
 print len(masses[2])
 
-#print masses[0]
-#h0 = subplots[0][0].hist(masses[0],100,range=(1.0,1.50),histtype='stepfilled',color='red',alpha=0.5)
-lh0 = lch.hist_err(masses[0],bins=100,range=(1.0,1.50),axes=subplots[0][0])
-subplots[0][0].set_xlabel(r"Invariant mass of the $p \pi^{-}$")
+print "Subcategories"
+#m=masses[0]
+print len(masses[0][masses[0]>1.100 and masses[0]<1.130])
 
+print 'Bad masses'
+print len(bad_masses[0])
+print len(bad_masses[1])
+print len(bad_masses[2])
 
-#h1 = subplots[0][1].hist(masses[1],100,range=(-0.1,0.10),histtype='stepfilled',color='blue')
-lh1 = lch.hist_err(masses[1],bins=100,range=(-0.1,0.10),axes=subplots[0][1])
-subplots[0][1].set_xlabel('Total missing mass')
+lh = []
 
-#h2 = subplots[0][2].hist(masses[2],100,range=(1.0,1.50),histtype='stepfilled',color='grey')
-lh2 = lch.hist_err(masses[2],bins=100,range=(1.0,1.50),axes=subplots[0][2])
-subplots[0][2].set_xlabel(r"Missing mass off the $K^{+}$")
+# Correct assignments
+lh.append(lch.hist_err(masses[0],bins=100,range=(1.0,1.50),axes=subplots[0][0]))
+subplots[0][0].set_xlabel(mtitles[0])
+subplots[0][0].set_xlim(1.0,1.50)
+
+lh.append(lch.hist_err(masses[1],bins=100,range=(-0.1,0.10),axes=subplots[0][1]))
+subplots[0][1].set_xlabel(mtitles[1])
+subplots[0][1].set_xlim(-0.1,0.1)
+
+lh.append(lch.hist_err(masses[2],bins=100,range=(1.0,1.50),axes=subplots[0][2]))
+subplots[0][2].set_xlabel(mtitles[2])
+subplots[0][2].set_xlim(1.0,1.50)
+
+# Incorrect assignments
+lh.append(lch.hist_err(bad_masses[0],bins=100,range=(1.0,1.50),axes=subplots[1][0]))
+subplots[1][0].set_xlabel(mtitles[0])
+subplots[1][0].set_xlim(1.0,1.50)
+
+lh.append(lch.hist_err(bad_masses[1],bins=100,range=(-0.1,0.10),axes=subplots[1][1]))
+subplots[1][1].set_xlabel(mtitles[1])
+subplots[1][1].set_xlim(-0.1,0.1)
+
+lh.append(lch.hist_err(bad_masses[2],bins=100,range=(1.0,1.50),axes=subplots[1][2]))
+subplots[1][2].set_xlabel(mtitles[2])
+subplots[1][2].set_xlim(1.0,1.50)
 
 #h3 = subplots[1][0].hist(vtxs[0],100,range=(0.0,20.00),histtype='stepfilled',color='red',alpha=0.5)
 #subplots[1][0].set_xlabel(r"Secondary vertex displacement $p \pi^{-}$")
@@ -107,7 +143,30 @@ subplots[0][2].set_xlabel(r"Missing mass off the $K^{+}$")
 #h4 = subplots[1][1].hist(vtxs[1],100,range=(0.0,20.00),histtype='stepfilled',color='red',alpha=0.5)
 #subplots[1][1].set_xlabel(r"Secondary vertex displacement $p \pi^{-}$")
 
-figs[0].savefig("k_lambda.png")
+lh2d0 = lch.hist_2D(masses[0],masses[2],xbins=100,ybins=100,xrange=[0.90,1.40],yrange=[0.90,1.40],axes=subplots[2][0])
+subplots[2][0].set_xlabel(mtitles[0])
+subplots[2][0].set_ylabel(mtitles[2])
+#subplots[2][0].set_aspect('auto')
+#figs[2].add_subplot(1,3,1)
+#plt.colorbar(cax=subplots[2][0])
+
+lh2d1 = lch.hist_2D(masses[0],masses[1],xbins=100,ybins=100,xrange=[0.90,1.40],yrange=[-0.10,0.1],axes=subplots[2][1])
+subplots[2][1].set_xlabel(mtitles[0])
+subplots[2][1].set_ylabel(mtitles[1])
+#subplots[2][1].set_aspect('auto')
+#figs[2].add_subplot(1,3,2)
+#plt.colorbar()
+
+lh2d2 = lch.hist_2D(masses[1],masses[2],xbins=100,ybins=100,xrange=[-0.10,0.1],yrange=[0.90,1.40],axes=subplots[2][2])
+subplots[2][2].set_xlabel(mtitles[1])
+subplots[2][2].set_ylabel(mtitles[2])
+#subplots[2][2].set_aspect('auto')
+#figs[2].add_subplot(1,3,3)
+#plt.colorbar()
+
+figs[0].savefig("Plots/fig0.png")
+figs[1].savefig("Plots/fig1.png")
+figs[2].savefig("Plots/fig2.png")
 
 plt.show()
 
