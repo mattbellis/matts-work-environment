be26037a37aa0dc623080d955984889f17e866de
Matthew Bellis <matthew.bellis@gmail.com>
2012-06-04 14:07:11 -0500

    Added an mlpy directory.

--- bin_scripts/sync_NIU_directory.sh
+++ bin_scripts/sync_NIU_directory.sh
@@ -7,25 +7,27 @@ endif
 
 
 if ( $1 == "push" ) then
-    rsync -P -r -u -a -v /home/bellis/printer_drivers \
-                        /home/bellis/bluehost_staging \
-                        /home/bellis/Stanford \
-                        /home/bellis/Siena \
-                        /home/bellis/wallpapers \
-                        /home/bellis/eBooks \
-                        /home/bellis/Work \
-                        /home/bellis/papers \
-                        /home/bellis/latex_stuff \
-                        /home/bellis/BaBar \
-                        /home/bellis/Jobs  \
-                        /home/bellis/stuff \
-                        /home/bellis/NIU \
-                        /home/bellis/Talks \
-                        /home/bellis/python_packages \
-                        mattbellis.dyndns-home.com:/home/bellis
+    rsync -P -r -u -a -v \
+        --exclude '*.npy' \
+        /home/bellis/printer_drivers \
+        /home/bellis/bluehost_staging \
+        /home/bellis/Stanford \
+        /home/bellis/Siena \
+        /home/bellis/wallpapers \
+        /home/bellis/eBooks \
+        /home/bellis/Work \
+        /home/bellis/papers \
+        /home/bellis/latex_stuff \
+        /home/bellis/BaBar \
+        /home/bellis/Jobs  \
+        /home/bellis/stuff \
+        /home/bellis/NIU \
+        /home/bellis/Talks \
+        /home/bellis/python_packages \
+        mattbellis.dyndns-home.com:/home/bellis
 
 else if ( $1 == "pull" ) then
-    rsync -P -r -u -a -v mattbellis.dyndns-home.com:/home/bellis/{Talks,Work,NIU,BaBar,Jobs,stuff,papers,latex_stuff,bluehost_staging,Stanford,Siena,python_packages,eBooks,wallpapers,printer_drivers} \
+    rsync -P -r -u -a -v --exclude '*.npy' mattbellis.dyndns-home.com:/home/bellis/{Talks,Work,NIU,BaBar,Jobs,stuff,papers,latex_stuff,bluehost_staging,Stanford,Siena,python_packages,eBooks,wallpapers,printer_drivers} \
         /home/bellis
 
 else
