7f60d2ecc5647e73873e210e6833592435b73cee
Matthew Bellis <matthew.bellis@gmail.com>
2012-06-30 23:45:15 -0500

    Working with new PDFs and no Monte Carlo normalization.

--- python/CoGeNT/fitting_utilities.py
+++ python/CoGeNT/fitting_utilities.py
@@ -113,6 +113,94 @@ def fitfunc(data,p,parnames,params_dict):
         pdf *= num_flat
         tot_pdf += pdf
 
+    elif flag==1:
+
+        max_val = 0.86786
+        threshold = 0.345
+        sigmoid_sigma = 0.241
+
+        efficiency = lambda x: sigmoid(x,threshold,sigmoid_sigma,max_val)
+
+
+
+        x = data[0]
+        y = data[1]
+
+        xlo = params_dict['var_e']['limits'][0]
+        xhi = params_dict['var_e']['limits'][1]
+        ylo = params_dict['var_t']['limits'][0]
+        yhi = params_dict['var_t']['limits'][1]
+
+        tot_pdf = np.zeros(len(x))
+        
+        e_exp0 = p[pn.index('e_exp0')]
+        num_exp0 = p[pn.index('num_exp0')]
+        num_flat = p[pn.index('num_flat')]
+        e_exp1 = p[pn.index('e_exp1')]
+        num_exp1 = p[pn.index('num_exp1')]
+
+        # Normalize numbers.
+        num_tot = 0.0
+        for name in pn:
+            if 'num_' in name or 'ncalc' in name:
+                num_tot += p[pn.index(name)]
+
+        '''
+        for name in pn:
+            if 'num_' in name or 'ncalc' in name:
+                p[pn.index(name)] /= num_tot
+        '''
+        num_exp0 /= num_tot
+        num_exp1 /= num_tot
+        num_flat /= num_tot
+
+
+        #means,sigmas,num_decays,num_decays_in_dataset,decay_constants = lshell_data(442)
+        #lshells = lshell_peaks(means,sigmas,num_decays_in_dataset)
+
+        means = []
+        sigmas = []
+        numls = []
+        decay_constants = []
+
+        for i in xrange(10):
+            name = "ls_mean%d" % (i)
+            means.append(p[pn.index(name)])
+            name = "ls_sigma%d" % (i)
+            sigmas.append(p[pn.index(name)])
+            name = "ls_ncalc%d" % (i)
+            numls.append(p[pn.index(name)]/num_tot) # Normalized this
+                                                    # to number of events.
+            name = "ls_dc%d" % (i)
+            decay_constants.append(p[pn.index(name)])
+
+        for n,m,s,dc in zip(numls,means,sigmas,decay_constants):
+            pdf  = pdfs.gauss(x,m,s,xlo,xhi,efficiency=efficiency)
+            #dc = -1.0/dc
+            dc = -1.0*dc
+            pdf *= pdfs.exp(y,dc,ylo,yhi)
+            pdf *= n
+            tot_pdf += pdf
+
+        # Exponential in energy
+        pdf  = pdfs.poly(y,[],ylo,yhi)
+        pdf *= pdfs.exp(x,e_exp0,xlo,xhi,efficiency=efficiency)
+        pdf *= num_exp0
+        tot_pdf += pdf
+
+        # Second exponential in energy
+        pdf  = pdfs.poly(y,[],ylo,yhi)
+        pdf *= pdfs.exp(x,e_exp1,xlo,xhi,efficiency=efficiency)
+        pdf *= num_exp1
+        tot_pdf += pdf
+
+        # Flat term
+        #print xlo,xhi,ylo,yhi
+        pdf  = pdfs.poly(y,[],ylo,yhi)
+        pdf *= pdfs.poly(x,[],xlo,xhi,efficiency=efficiency)
+        pdf *= num_flat
+        tot_pdf += pdf
+
     return tot_pdf
 ################################################################################
 
@@ -254,9 +342,10 @@ class Minuit_FCN:
         #print self.func_code.co_varnames
         #flag = arg[0]
         data0 = self.data[0]
-        mc = self.data[1]
+        #mc = self.data[1]
 
-        val = emlf_minuit(data0,mc,arg,self.func_code.co_varnames,self.params_dict)
+        #val = emlf_minuit(data0,mc,arg,self.func_code.co_varnames,self.params_dict)
+        val = emlf_normalized_minuit(data0,arg,self.func_code.co_varnames,self.params_dict)
 
         return val
 
@@ -303,5 +392,22 @@ def emlf_minuit(data,mc,p,parnames,params_dict):
 
     return ret
 
+
+
+################################################################################
+# Extended maximum likelihood function for minuit, normalized already.
+################################################################################
+def emlf_normalized_minuit(data,p,parnames,params_dict):
+
+    ndata = len(data[0])
+
+    n = 0
+    for name in parnames:
+        if 'num_' in name:
+            n += p[parnames.index(name)]
+
+    ret = (-np.log(fitfunc(data,p,parnames,params_dict))).sum() - pois(n,ndata)
+
+    return ret
 
 
