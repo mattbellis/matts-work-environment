8c35b89453f1541cc148d738f2ca3ec87cc2c402
Matthew Bellis <matthew.bellis@gmail.com>
2012-07-03 13:07:40 -0500

    Added plot_effects_of_cuts.py

---
+++ python/McCracken_BNV_analysis/plot_effects_of_cuts.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+import sys
+
+sigf = np.load(sys.argv[1])
+bkgf = np.load(sys.argv[2])
+
+nsig = sigf[1]
+nbkg = bkgf[1]
+
+nsig_surv = sigf[2]
+nbkg_surv = bkgf[2]
+
+sig_eff = (nsig_surv/nsig)
+bkg_rej = 1.0 - (nbkg_surv/nbkg)
+
+print sig_eff
+
+
+plt.figure()
+print len(bkg_rej),len(sig_eff)
+plt.plot(bkg_rej,sig_eff,'o')
+plt.xlabel('Background rejection (fractional)')
+plt.ylabel('Signal efficiency (fractional)')
+
+plt.figure()
+#fom_cutoff = 0.324 # For a=3
+#fom_cutoff = 0.194 # For a=5
+#for a,fmt in zip([3,4,5],['bo','go','ro']):
+for a,fmt in zip([5],['ro']):
+    bkg_scale_factor = 1.0
+    fom = sig_eff/(np.sqrt(bkg_scale_factor*nbkg_surv) + a/2.0)
+    fom_sorted = np.array(fom)
+    fom_sorted.sort()
+    print "here"
+    print fom_sorted
+    fom_cutoff = fom_sorted[-6]
+    print "fom_cutoff: ",fom_cutoff
+    plt.plot(fom,sig_eff,fmt)
+    #plt.plot(fom,nbkg_surv,'o')
+    if a==5:
+        print fom[fom>fom_cutoff]
+        print sig_eff[fom>fom_cutoff]
+        print bkg_rej[fom>fom_cutoff]
+        for i in xrange(7):
+            print sigf[0][i][fom>fom_cutoff]
+
+#title = "Punzi figure of merit (a=%d)" % (a)
+title = "Punzi figure of merit"
+plt.xlabel(title)
+plt.ylabel('Signal efficiency (fractional)')
+
+plt.show()
