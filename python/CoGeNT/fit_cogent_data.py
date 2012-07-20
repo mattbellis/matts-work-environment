8830cac934caaa8db52dd1081fb17549470ed560
Matthew Bellis <matthew.bellis@gmail.com>
2012-07-20 18:08:49 -0500

    Implemented the Sagitarius stream.

--- python/CoGeNT/fit_cogent_data.py
+++ python/CoGeNT/fit_cogent_data.py
@@ -189,7 +189,7 @@ def main():
     params_dict['num_exp0'] = {'fix':False,'start_val':296.0,'limits':(0.0,10000.0)}
 
     # Use the dark matter SHM, WIMPS
-    if args.fit==2 or args.fit==3: 
+    if args.fit==2 or args.fit==3 or args.fit==4: 
         params_dict['num_exp0'] = {'fix':True,'start_val':1.0,'limits':(0.0,10000.0)}
         params_dict['mDM'] = {'fix':True,'start_val':7.00,'limits':(5.0,20.0)}
         params_dict['sigma_n'] = {'fix':True,'start_val':1e-40,'limits':(1e-42,1e-38)}
@@ -289,24 +289,26 @@ def main():
         tot_sr_typts = [tot + y for tot,y in zip(tot_sr_typts,sr_typts)]
 
     # Plot wimp term
-    if args.fit==2 or args.fit==3:
+    if args.fit==2 or args.fit==3 or args.fit==4:
+        wimp_model = None
+        if args.fit==2:
+            wimp_model = 'shm'
+        elif args.fit==3:
+            wimp_model = 'debris'
+        elif args.fit==4:
+            wimp_model = 'stream'
+
         num_wimps = 0.0
         for sr in subranges[1]:
-            #num_wimps += integrate.dblquad(wimp,0.5,3.2,lambda x:sr[0],lambda x:sr[1],args=(AGe,values['mDM'],values['sigma_n'],efficiency),epsabs=dblqtol)[0]*(0.333)
-            num_wimps += integrate.dblquad(wimp_debris,0.5,3.2,lambda x:sr[0],lambda x:sr[1],args=(AGe,values['mDM'],values['sigma_n'],efficiency),epsabs=dblqtol)[0]*(0.333)
+            num_wimps += integrate.dblquad(wimp,0.5,3.2,lambda x:sr[0],lambda x:sr[1],args=(AGe,values['mDM'],values['sigma_n'],efficiency,wimp_model),epsabs=dblqtol)[0]*(0.333)
 
-        #func = lambda x: plot_wimp_er(x,AGe,values['mDM'],values['sigma_n'],time_range=[1,459])
-        func = lambda x: plot_wimp_debris_er(x,AGe,values['mDM'],values['sigma_n'],time_range=[1,459])
+        func = lambda x: plot_wimp_er(x,AGe,values['mDM'],values['sigma_n'],time_range=[1,459],model=wimp_model)
         srypts,plot,srxpts = plot_pdf_from_lambda(func,bin_width=bin_widths[0],scale=num_wimps,fmt='k-',linewidth=3,axes=ax0,subranges=[[0.5,3.2]],efficiency=efficiency)
-        #ax0.plot(srxpts[0],srypts[0],'k-',linewidth=3)
-        #print len(eytot),len(eypts)
         eytot += srypts[0]
 
-        #func = lambda x: plot_wimp_day(x,AGe,values['mDM'],values['sigma_n'],e_range=[0.5,3.2])
-        func = lambda x: plot_wimp_debris_day(x,AGe,values['mDM'],values['sigma_n'],e_range=[0.5,3.2])
+        func = lambda x: plot_wimp_day(x,AGe,values['mDM'],values['sigma_n'],e_range=[0.5,3.2],model=wimp_model)
         sr_typts,plot,sr_txpts = plot_pdf_from_lambda(func,bin_width=bin_widths[1],scale=num_wimps,fmt='k-',linewidth=3,axes=ax1,subranges=subranges[1])
         tot_sr_typts = [tot + y for tot,y in zip(tot_sr_typts,sr_typts)]
-        #ax1.plot(sr_txpts,sr_typts,'b',linewidth=3)
 
     ############################################################################
     # Second exponential
