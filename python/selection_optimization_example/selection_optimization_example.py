79f415e5d582c9c269627d0aea5a9d82906f5887
Matthew Bellis <matthew.bellis@gmail.com>
2013-02-20 18:26:30 -0500

    More mods

--- python/selection_optimization_example/selection_optimization_example.py
+++ python/selection_optimization_example/selection_optimization_example.py
@@ -74,6 +74,9 @@ plt.xlabel(r'$\tau$ (arbitrary)',fontsize=24)
 plt.ylabel(r'# events',fontsize=24)
 name = "Plots/optstudy_bkgy0%s.png" % (tag); plt.savefig(name)
 
+
+
+
 ################################################################################
 # Plot the combined signal and background
 ################################################################################
@@ -95,10 +98,6 @@ plt.xlim(0,3.0)
 plt.subplots_adjust(top=0.95,bottom=0.15,right=0.95,left=0.15)
 plt.xlabel(r'$\tau$ (arbitrary)',fontsize=24)
 plt.ylabel(r'# events',fontsize=24)
-# Draw the line and arrow
-ymax = plt.ylim()[1]
-plt.plot([2.0,2.0],[0,ymax],'r-')
-plt.arrow(2.0-0.05,ymax/2.0,-0.5,0,lw=3,head_width=ymax/20,head_length=0.05,fc='k',ec='k')
 name = "Plots/optstudy_bothy0%s.png" % (tag); plt.savefig(name)
 
 ################################################################################
@@ -112,19 +111,24 @@ mpts0 = []
 mpts1 = []
 mpts2 = []
 
-for i in np.arange(3.0,0.1,-0.05):
+k = 0
+for i in np.arange(3.0,0.1,-0.10):
     #print i,len(sig_y[sig_y>i])
     index0 = sig_y<i
     index1 = sig_x>0.87
     index2 = sig_x<0.92
     index = index0*index1*index2
-    nsig_surv = len(sig_y[index])
+    sig_surv_x = sig_x[index]
+    sig_surv_y = sig_y[index]
+    nsig_surv = len(sig_surv_y)
 
     index0 = bkg_y<i
     index1 = bkg_x>0.87
     index2 = bkg_x<0.92
     index = index0*index1*index2
-    nbkg_surv = len(bkg_y[index])
+    bkg_surv_x = bkg_x[index]
+    bkg_surv_y = bkg_y[index]
+    nbkg_surv = len(bkg_surv_y)
 
     print nbkg_surv,nsig_surv
 
@@ -135,6 +139,75 @@ for i in np.arange(3.0,0.1,-0.05):
     mpts0.append(nsig_surv/np.sqrt(nsig_surv+nbkg_surv))
     mpts1.append(nsig_surv/np.sqrt(nbkg_surv))
     mpts2.append((nsig_surv/float(nsig))/(np.sqrt(nbkg_surv) + (4.0/2.0)))
+
+    ############################################################################
+    # Make the plots
+    ############################################################################
+    ################################################################################
+    # Plot the signal
+    ################################################################################
+    plt.figure()
+    lch.hist_err(sig_x,bins=nbinsx,range=(0.8,1.0),linewidth=2)
+    lch.hist_err(sig_surv_x,bins=nbinsx,range=(0.8,1.0),linewidth=2,color='r',ecolor='r')
+    plt.subplots_adjust(top=0.95,bottom=0.15,right=0.95,left=0.15)
+    # Draw two lines around mass peak
+    ymax = plt.ylim()[1]
+    plt.plot([0.87,0.87],[0,ymax],'r-')
+    plt.plot([0.92,0.92],[0,ymax],'r-')
+    plt.xlabel(r'Mass (GeV/c$^2$)',fontsize=24)
+    plt.ylabel(r'# events',fontsize=24)
+    name = "Plots/optstudy_sigx_cuts%d_%s.png" % (k,tag); plt.savefig(name)
+
+    plt.figure()
+    lch.hist_err(sig_y,bins=nbinsy,range=(0,3.0),linewidth=2)
+    plt.xlim(0,3.0)
+    plt.subplots_adjust(top=0.95,bottom=0.15,right=0.95,left=0.15)
+    plt.xlabel(r'$\tau$ (arbitrary)',fontsize=24)
+    plt.ylabel(r'# events',fontsize=24)
+    # Draw the line and arrow
+    ymax = plt.ylim()[1]
+    plt.plot([i,i],[0,ymax],'r-')
+    plt.arrow(i-0.05,ymax/2.0,-0.5,0,lw=3,head_width=ymax/20,head_length=0.05,fc='k',ec='k')
+    name = "Plots/optstudy_sigy_cuts%d_%s.png" % (k,tag); plt.savefig(name)
+
+    ################################################################################
+    # Plot the background
+    ################################################################################
+    plt.figure()
+    lch.hist_err(bkg_x,bins=nbinsx,range=(0.8,1.0),linewidth=2)
+    lch.hist_err(bkg_surv_x,bins=nbinsx,range=(0.8,1.0),linewidth=2,color='r',ecolor='r')
+    plt.subplots_adjust(top=0.95,bottom=0.15,right=0.95,left=0.15)
+    # Draw two lines around mass peak
+    ymax = plt.ylim()[1]
+    plt.plot([0.87,0.87],[0,ymax],'r-')
+    plt.plot([0.92,0.92],[0,ymax],'r-')
+    plt.xlabel(r'Mass (GeV/c$^2$)',fontsize=24)
+    plt.ylabel(r'# events',fontsize=24)
+    name = "Plots/optstudy_bkgx_cuts%d_%s.png" % (k,tag); plt.savefig(name)
+
+    plt.figure()
+    lch.hist_err(bkg_y,bins=nbinsy,range=(0,3.0),linewidth=2)
+    plt.xlim(0,3.0)
+    plt.subplots_adjust(top=0.95,bottom=0.15,right=0.95,left=0.15)
+    plt.xlabel(r'$\tau$ (arbitrary)',fontsize=24)
+    plt.ylabel(r'# events',fontsize=24)
+    # Draw the line and arrow
+    ymax = plt.ylim()[1]
+    plt.plot([i,i],[0,ymax],'r-')
+    plt.arrow(i-0.05,ymax/2.0,-0.5,0,lw=3,head_width=ymax/20,head_length=0.05,fc='k',ec='k')
+    name = "Plots/optstudy_bkgy_cuts%d_%s.png" % (k,tag); plt.savefig(name)
+
+    k+=1
+
+    ################################################################################
+    # Make the plots of the efficiency
+    ################################################################################
+    plt.figure()
+    plt.plot(nb_pts,ns_pts,'ko')
+    plt.subplots_adjust(top=0.95,bottom=0.15,right=0.95,left=0.15)
+    plt.xlabel(r'Background rejection (fractional)',fontsize=24)
+    plt.ylabel(r'Signal efficiency (fractional)',fontsize=24)
+    name = "Plots/optstudy_sigbkgcurve%s.png" % (tag); plt.savefig(name)
 
 print ns_pts
 print nb_pts
