e76b8190369a5e697ed7c568e43afdaac0de78d8
Matthew Bellis <matthew.bellis@gmail.com>
2013-02-20 18:26:30 -0500

    More mods

--- python/nearest_neighbor_fitting/nnf.py
+++ python/nearest_neighbor_fitting/nnf.py
@@ -2,10 +2,61 @@ import matplotlib.pylab as plt
 import numpy as np
 import scipy.stats as stats
 
+import lichen.pdfs as pdfs
+import lichen.iminuit_fitting_utilities as fitutils
+import lichen.plotting_utilities as plotutils
+
+import iminuit as minuit
+
+################################################################################
+# CoGeNT fit
+################################################################################
+def fitfunc(data,p,parnames,params_dict):
+
+    tot_pdf = 0
+
+    return tot_pdf
+
+################################################################################
+# Extended maximum likelihood function for minuit, normalized already.
+################################################################################
+def emlf_normalized_minuit(data,p,parnames,params_dict):
+
+    ndata = len(data[0])
+
+    flag = p[parnames.index('flag')]
+
+    wimp_model = None
+
+    num_tot = 0.0
+    for name in parnames:
+        if 'num_' in name or 'ncalc' in name:
+            num_tot += p[parnames.index(name)]
+        elif 'num_flat' in name or 'num_exp1' in name:
+            num_tot += p[parnames.index(name)]
+
+    tot_pdf = fitfunc(data,p,parnames,params_dict)
+
+    likelihood_func = (-np.log(tot_pdf)).sum()
+
+    print num_tot,ndata
+
+    ret = likelihood_func - fitutils.pois(num_tot,ndata)
+
+    return ret
+
+################################################################################
+
+################################################################################
+# Generate signal
+################################################################################
 nsig = 200
 sig = stats.norm.rvs(loc=5,scale=1,size=nsig)
 
 
+################################################################################
+# Generate background
+################################################################################
 nbkg = 1000
 bkg = np.array([])
 while len(bkg)<nbkg:
@@ -18,13 +69,18 @@ while len(bkg)<nbkg:
 
 print len(bkg)
 
+################################################################################
+# Combine signal and background
+################################################################################
 data = sig.copy()
 data = np.append(data,bkg)
-
 
 plt.figure()
 plt.hist(data,bins=50)
 
+################################################################################
+# Generate the templates we will use to fit the data
+################################################################################
 template0 = stats.norm.rvs(loc=5,scale=1,size=500)
 
 nt1 = 2000
@@ -37,12 +93,15 @@ while len(template1)<nt1:
     remainder = nt1-len(template1)
     template1 = np.append(template1,temp[index][0:remainder])
 
-
 plt.figure()
 plt.hist(template0,bins=50,color='r')
 plt.figure()
 plt.hist(template1,bins=50,color='r')
 
+
+################################################################################
+# Calculate the distances from data to the templates.
+################################################################################
 distances_t0 = np.zeros(len(data))
 distances_t1 = np.zeros(len(data))
 for i,x in enumerate(data):
@@ -67,6 +126,37 @@ plt.figure()
 plt.hist(distances_t0,bins=50,color='g')
 plt.figure()
 plt.hist(distances_t1,bins=50,color='g')
+
+############################################################################
+# Declare the fit parameters
+############################################################################
+ranges = [[0,10]]
+
+params_dict = {}
+#params_dict['flag'] = {'fix':True,'start_val':args.fit}
+params_dict['flag'] = {'fix':True,'start_val':0}
+params_dict['var_x'] = {'fix':True,'start_val':0,'limits':(ranges[0][0],ranges[0][1])}
+
+ncomponents = 2
+for i,val in enumerate(ncomponents):
+    name = "num%d" % (i)
+    params_dict[name] = {'fix':False,'start_val':val,'limits':(0.0,6000.0)}
+
+params_names,kwd = fitutils.dict2kwd(params_dict)
+
+f = fitutils.Minuit_FCN([data],params_dict,emlf_normalized_minuit)
+
+m = minuit.Minuit(f,**kwd)
+
+# For maximum likelihood method.
+m.up = 0.5
+
+# Up the tolerance.
+m.tol = 1.0
+
+m.migrad()
+
+values = m.values
 
 
 plt.show()
