4b04349a89d437ba51bc57acbf3830691195664e
Matthew Bellis <matthew.bellis@gmail.com>
2014-01-07 11:44:51 -0500

    Split by z is mostly working.

--- C/splitting_mice_dataset/split_file_into_many_files_by_z.c
+++ C/splitting_mice_dataset/split_file_into_many_files_by_z.c
@@ -9,8 +9,10 @@ int main(int argc, char **argv)
     char outfilename[256];
 
     FILE *ifp = fopen(infilename, "r");
-    FILE *ofp = NULL;
+    FILE *ofp[256];
     float ra,dec,z;
+    float z_v,x_c,y_c,z_c;
+    char dummy[256];
 
     if (ifp == NULL) {
         printf("Can't open input file %s!",infilename);
@@ -19,13 +21,32 @@ int main(int argc, char **argv)
 
     int count = 0;
     int filecount = 0;
-    int nentries_per_file = 100000;
+    float zwidth = 0.01;
+    float zmin = 0.0;
+    float zmax = 1.5;
+    float iz = 0;
 
-    while (fscanf(ifp, "%f,%f,%f",&ra,&dec,&z) != EOF) {
+    filecount = 0;
+    for (iz=zmin;iz+=zwidth;iz<zmax)
+    {
+        sprintf(outfilename,"smaller_file_zlo%3.2f_zhi%3.2f.dat",iz,iz+zwidth);
+        printf("Opening %s\n",outfilename);
+        ofp[filecount] = fopen(outfilename, "w");
+        filecount += 1;
+    }
+
+    exit(0);
+
+    // For the big file.
+    fscanf(ifp, "%s\t%s\t%s\t%s\t%s\t%s\t%s",&dummy,&dummy,&dummy,&dummy,&dummy,&dummy,&dummy);
+
+    //while (fscanf(ifp, "%f,%f,%f",&ra,&dec,&z) != EOF) {
+    while (fscanf(ifp, "%f\t%f\t%f\t%f\t%f\t%f\t%f",&ra,&dec,&z,&z_v,&x_c,&y_c,&z_c) != EOF) {
         //printf("%f %f %f\n",ra,dec,z);
+        /*
         if (count%nentries_per_file==0)
         {
-            sprintf(outfilename,"outfile_nentries%d_%04d.dat",nentries_per_file,filecount);
+            sprintf(outfilename,"smaller_file_nentries%d_%04d.dat",nentries_per_file,filecount);
             printf("Opening %s\n",outfilename);
             ofp = fopen(outfilename, "w");
             if (ofp == NULL) {
@@ -34,20 +55,22 @@ int main(int argc, char **argv)
             }
             filecount += 1;
         }
-        fprintf(ofp,"%f %f %f\n",ra,dec,z);
+        */
+        //fprintf(ofp,"%f %f %f\n",ra,dec,z);
+        fprintf(ofp[count],"%f %f %f %f %f %f %f\n",ra,dec,z,z_v,x_c,y_c,z_c);
 
         if (count%100000==0)
             printf("%d\n",count);
 
         count += 1;
-        if (count%nentries_per_file==nentries_per_file-1)
-        {
-            close(ofp);
-        }
+        //if (count%nentries_per_file==nentries_per_file-1)
+        //{
+            //close(ofp);
+        //}
     }
 
     fclose(ifp);
-    fclose(ofp);
+    //fclose(ofp);
 
     return 0;
 }
