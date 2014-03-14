b61f7eeae9e53c3dffb78cf4a615c2051547af63
Matthew Bellis <matthew.bellis@gmail.com>
2014-03-14 14:06:24 -0400

    Some sample plot stuff.

--- python/dm_observations_and_exclusions/sample_plot.py
+++ python/dm_observations_and_exclusions/sample_plot.py
@@ -19,13 +19,21 @@ x2 = data_values[0][index]*data_values_rescale[0][0]
 y2 = data_values[0][index+1]*data_values_rescale[0][0] # Only has one rescaling
 label2 = data_label
 
+from python_files.SuperCDMS_2014 import *
+index = np.arange(0,len(data_values[0]),2)
+x3 = data_values[0][index]*data_values_rescale[0][0]
+y3 = data_values[0][index+1]*data_values_rescale[0][0] # Only has one rescaling
+label3 = data_label
+
 plt.plot(x0,y0,color='red',label=label0)
 plt.plot(x1,y1,color='green',label=label1)
 plt.plot(x2,y2,color='blue',label=label2)
+plt.plot(x3,y3,color='yellow',label=label3)
 
 plt.fill_between(x0,y0,max(y2),facecolor='red',alpha=0.1)
 plt.fill(x1,y1,facecolor='green',alpha=0.9)
 plt.fill_between(x2,y2,max(y2),facecolor='blue',alpha=0.1)
+plt.fill_between(x3,y3,max(y3),facecolor='yellow',alpha=0.1)
 
 plt.yscale('log')
 plt.xlim(0,30)
