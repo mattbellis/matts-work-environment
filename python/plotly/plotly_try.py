aa028da449ed3c7dd815dbcc5195afcef9bc71f8
Matthew Bellis <matthew.bellis@gmail.com>
2014-06-09 13:50:11 -0400

    Made it so that the plotly example doesn't keep generating a new plot.

--- python/plotly/plotly_try.py
+++ python/plotly/plotly_try.py
@@ -4,6 +4,6 @@ py = plotly.plotly(username_or_email="MatthewBellis", key="d6h4et78v5")
 
 x0 = [1,2,3,4]; y0 = [10,15,13,17]
 x1 = [2,3,4,5]; y1 = [16,5,11,9]
-response = py.plot(x0, y0, x1, y1)
+response = py.plot(x0, y0, x1, y1,filename='scatterplot_example',fileopt='overwrite')
 url = response['url']
 filename = response['filename']
