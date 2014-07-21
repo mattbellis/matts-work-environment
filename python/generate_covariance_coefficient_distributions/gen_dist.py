e3937cee07de71202a6d072a4e9287a0f0c4bbf7
Matthew Bellis <matthew.bellis@gmail.com>
2014-07-21 17:22:52 -0400

    Minor changes.

--- python/generate_covariance_coefficient_distributions/gen_dist.py
+++ python/generate_covariance_coefficient_distributions/gen_dist.py
@@ -2,12 +2,22 @@ import matplotlib.pylab as plt
 import numpy as np
 
 mean = [0,0]
-cov = [[1,0.2],[0.2,1]] # diagonal covariance, points lie on x or y-axis
+cov = [[1,-0.9],[-0.9,1]] # diagonal covariance, points lie on x or y-axis
 
-x,y = np.random.multivariate_normal(mean,cov,10000).T
+x,y = np.random.multivariate_normal(mean,cov,1000).T
 
+plt.figure()
 plt.plot(x,y,'o')
 plt.axis('equal')
+
+plt.figure()
+plt.hist(x,bins=25)
+
+plt.figure()
+plt.hist(y,bins=25)
+
+print x
+print y
 
 zipped = zip(x,y)
 np.savetxt('dataset4.dat',zipped,fmt='%f %f')
