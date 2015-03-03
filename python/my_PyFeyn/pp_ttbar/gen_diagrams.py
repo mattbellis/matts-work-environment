5ebc626e563a1e6d5b69e69899b1854b9dd3270a
Matthew Bellis <matthew.bellis@gmail.com>
2015-03-03 01:18:03 -0500

    Added pp to ttbar BNV.

--- python/my_PyFeyn/pp_ttbar/gen_diagrams.py
+++ python/my_PyFeyn/pp_ttbar/gen_diagrams.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+'''
 from pp_ttbar_boosted import *
 tag = "_blk_bkg"
 
 for i in range(0,1):
     print i
     pp_ttbar_boosted("pp_ttbar_boosted"+tag,i)
+'''
+
+from pp_ttbar_BNV import *
+tag = "_blk_bkg"
+
+for i in range(0,1):
+    print i
+    pp_ttbar_BNV("pp_ttbar_BNV"+tag,i)
 
