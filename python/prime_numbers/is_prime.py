cf01a91bfff233c238d1693b0c6dcc84b057d7e7
Matthew Bellis <matthew.bellis@gmail.com>
2015-12-18 01:39:59 -0500

    Minor changes.

--- python/prime_numbers/is_prime.py
+++ python/prime_numbers/is_prime.py
@@ -1,8 +1,20 @@
 def is_prime(n):
-    for i in range(2,int(n/2)+2):
+    #for i in range(2,int(n/2)+2):
+    i = 2
+    while i < int(n/i)+2:
         if n%i==0:
             #print('not prime')
             return False # is not prime
+        i+= 1
     return True # is prime
+
+
+def is_square(n):
+    from math import sqrt 
+    a = int(sqrt(n))
+    if a*a == n:
+        return True # is a perfect square
+    else:
+        return False
 
 
