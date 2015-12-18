69a12b034d7fafa34a43fa3f1cb407ce6917b80a
Matthew Bellis <matthew.bellis@gmail.com>
2015-12-17 22:54:08 -0500

    Minor changes.

--- python/prime_numbers/check_numbers.py
+++ python/prime_numbers/check_numbers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 from is_prime import is_prime
 
@@ -6,7 +7,7 @@ import sys
 
 # Gen list of primes
 
-numbers = np.arange(3,1000,1)
+numbers = np.arange(3,2000,1)
 
 primes = []
 for n in numbers:
@@ -15,30 +16,77 @@ for n in numbers:
 
 #print primes
 
-#nprimes = len(primes)
+nprimes = len(primes)
+
+print nprimes 
+
 nprimes = int(sys.argv[1])
+#maxprime = int(sys.argv[1])
 
-maxprime = nprimes
+ntots = []
+nworks = []
+maxprimes = []
 
-nwork = 0
-ntot = 0
-for i in range(0,maxprime-1):
-    for j in range(i+1,maxprime):
-        a = primes[i]
-        b = primes[j]
+for maxprime in range(10,nprimes,10):
 
-        c = (a*b) - (b-a)
+    nwork = 0
+    ntot = 0
+    for i in range(0,maxprime-1):
+        for j in range(i+1,maxprime):
+            a = primes[i]
+            b = primes[j]
 
-        val = is_prime(c)
+            c = (a*b) - (b-a)
 
-        if val:
-            nwork += 1
+            val = is_prime(c)
 
-        #print "%4d %4d %7d %s" % (a,b,c,val)
-        ntot += 1
+            if val:
+                nwork += 1
 
-print nprimes
-print ntot
-print nwork
-print ntot-nwork
-print float(nwork)/ntot
+            #print "%4d %4d %7d %s" % (a,b,c,val)
+            ntot += 1
+
+    print "---------"
+    print primes
+    print (len(primes))
+    print maxprime
+    print nprimes
+    print ntot
+    print nwork
+    print ntot-nwork
+    print float(nwork)/ntot
+    
+
+    maxprimes.append(maxprime)
+    ntots.append(ntot)
+    nworks.append(nwork)
+
+nworks = np.array(nworks)
+ntots = np.array(ntots)
+
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,3,1)
+plt.plot(ntots,nworks,'bo')
+plt.xlabel("# total")
+plt.ylabel("# prime")
+
+plt.subplot(1,3,2)
+plt.plot(maxprimes,nworks,'bo')
+plt.xlim(-10,max(maxprimes)+10)
+plt.xlabel("# max primes")
+plt.ylabel("# prime")
+
+plt.subplot(1,3,3)
+plt.plot(maxprimes,nworks.astype(float)/ntots,'bo')
+plt.xlim(-10,max(maxprimes)+10)
+plt.ylim(0)
+plt.xlabel("# max primes")
+plt.ylabel("fraction that are prime")
+
+name = 'numbers_%d.png' % (nprimes)
+plt.savefig(name)
+
+plt.tight_layout()
+
+plt.show()
