9559cda2fe062c4971fc7bab49c06dd01fede622
Matt Bellis <matthew.bellis@gmail.com>
2016-02-23 16:38:59 -0500

    Added some stuff to the CoGeNT cutaway image.

--- python/cogent_diagrams/diagram.py
+++ python/cogent_diagrams/diagram.py
@@ -12,10 +12,13 @@ ax = fig.add_subplot(1,1,1)
 p = patches.Ellipse((0,0.5), 1.4, 0.4,facecolor='gray',alpha=0.8)
 ax.add_patch(p)
 
+p = patches.Ellipse((0,0.5), 0.17, 0.05,facecolor='yellow',alpha=0.8)
+ax.add_patch(p)
+
 p = patches.Rectangle((-0.7,-0.5), 1.4, 1.0,facecolor='black',alpha=1.0)#, ec="none")
 ax.add_patch(p)
 
-p = patches.Rectangle((-0.7+0.05,-0.5+0.05), 1.4-0.10, 1.0-0.10,facecolor='yellow',alpha=1.0)#, ec="none")
+p = patches.Rectangle((-0.7+0.05,-0.5+0.05), 1.4-0.10, 1.0-0.10,facecolor='blue',alpha=1.0)#, ec="none")
 ax.add_patch(p)
 
 p = patches.Rectangle((-0.7+0.10,-0.5+0.10), 1.4-0.20, 1.0-0.20,facecolor='white',alpha=1.0)#, ec="none")
