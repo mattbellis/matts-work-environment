a056f6a2c4e32a12539a8345547e8ac03ac7f02b
Matthew Bellis <matthew.bellis@gmail.com>
2017-02-02 01:57:31 -0500

    Changed fontsize in plots.

--- python/Siena_classes/PHYS_110_General_Physics_I/gen_xva_curves.py
+++ python/Siena_classes/PHYS_110_General_Physics_I/gen_xva_curves.py
@@ -1,16 +1,21 @@
 import numpy as np
 import matplotlib.pylab as plt
+import matplotlib
 
 import matplotlib as mpl
 mpl.rcParams['grid.linewidth'] = 3
 
-'''
+matplotlib.rc('xtick', labelsize=20) 
+matplotlib.rc('ytick', labelsize=20) 
+
+
+#'''
 t_intervals = [0,2,5,6,8]
 a_intervals = [0,-20,-5,15]
 v0 = 20.0
 x0 = 0
 tag = "A"
-'''
+#'''
 
 '''
 t_intervals = [0,2,5,6,8]
@@ -20,13 +25,13 @@ x0 = 10
 tag = "B"
 '''
 
-#'''
+'''
 t_intervals = [0,2,5,6,8]
 a_intervals = [10,-10,0,5]
 v0 = -10.0
 x0 = 0
 tag = "C"
-#'''
+'''
 
 dt = 0.01
 t = np.arange(0,t_intervals[-1],dt)
@@ -37,7 +42,7 @@ x = np.ones(len(t))
 v[0] = v0
 x[0] = x0
 
-for i in xrange(len(t_intervals)-1):
+for i in range(0,len(t_intervals)-1):
     idx0 = t>t_intervals[i]
     idx1 = t<=t_intervals[i+1]
     a[idx0*idx1] = a_intervals[i]
@@ -56,6 +61,7 @@ plt.ylim(min(a)-1,max(a)+1)
 plt.xlabel("Time (s)",fontsize=24)
 plt.ylabel(r"Acceleration (m/s$^2$)",fontsize=24)
 plt.grid()
+plt.tight_layout()
 name = "acceleration_%s.png" % (tag)
 plt.savefig(name)
 
@@ -65,6 +71,7 @@ plt.ylim(min(v)-1,max(v)+1)
 plt.xlabel("Time (s)",fontsize=24)
 plt.ylabel("Velocity (m/s)",fontsize=24)
 plt.grid()
+plt.tight_layout()
 name = "velocity_%s.png" % (tag)
 plt.savefig(name)
 
@@ -74,9 +81,10 @@ plt.ylim(min(x)-1,max(x)+1)
 plt.xlabel("Time (s)",fontsize=24)
 plt.ylabel("Position (m)",fontsize=24)
 plt.grid()
+plt.tight_layout()
 name = "position_%s.png" % (tag)
 plt.savefig(name)
 
-#plt.show()
+plt.show()
 
 
