cf273af026020107f5ce76c34d86b58d597d8440
Matt Bellis <matthew.bellis@gmail.com>
2017-04-03 15:02:33 -0400

    Minimal stuff.

--- bin_scripts/cmake_for_ROOT.sh
+++ bin_scripts/cmake_for_ROOT.sh
@@ -1,7 +1,7 @@
 cmake ../root/ \
         -DCMAKE_INSTALL_PREFIX=/usr/local/ \
-        -DPYTHON_INCLUDE_DIR=/opt/anaconda3/include/python3.5m \
-        -DPYTHON_LIBRARY=/opt/anaconda3/lib/libpython3.5m.so \
+        -DPYTHON_INCLUDE_DIR=/opt/anaconda3/include/python3.6m \
+        -DPYTHON_LIBRARY=/opt/anaconda3/lib/libpython3.6m.so \
         -Dgnuinstall=ON
 #sudo cmake --build . --target install -- -j4
 
