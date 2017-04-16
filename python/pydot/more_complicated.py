4e5b76bde118ff09d5d51a4dc944bebf3b1338ef
mattbellis <matthew.bellis@gmail.com>
2017-04-16 11:06:14 -0400

    First commit of some dot

---
+++ python/pydot/more_complicated.py
@@ -0,0 +1,25 @@
+import pydot
+
+graph = pydot.Dot(graph_type='graph')
+
+# creating nodes is as simple as creating edges!
+node_a = pydot.Node(name="nodea", style="filled", fillcolor="red",texlbl=r'$\omega$',label='dummy')
+
+node_b = pydot.Node("Node B", style="filled", fillcolor="green")
+node_c = pydot.Node("Node C", style="filled", fillcolor="#0000ff")
+node_d = pydot.Node("Node D", style="filled", fillcolor="#976856")
+
+graph.add_node(node_a)
+graph.add_node(node_b)
+graph.add_node(node_c)
+graph.add_node(node_d)
+
+graph.add_edge(pydot.Edge(node_a, node_b))
+graph.add_edge(pydot.Edge(node_b, node_c))
+graph.add_edge(pydot.Edge(node_c, node_d))
+# but, let's make this last edge special, yes?
+graph.add_edge(pydot.Edge(node_d, node_a, label="and back we go again", labelfontcolor="#009933", fontsize="10.0", color="blue"))
+
+# and we are done
+graph.write_png('complicated.png',prog='dot')
+
