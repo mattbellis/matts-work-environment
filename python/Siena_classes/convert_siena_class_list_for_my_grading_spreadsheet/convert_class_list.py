c248aa79428c80ba6756418e2590c3ead1d710fd
mattbellis <matthew.bellis@gmail.com>
2017-09-06 22:09:11 -0400

    Stuff.

--- python/Siena_classes/convert_siena_class_list_for_my_grading_spreadsheet/convert_class_list.py
+++ python/Siena_classes/convert_siena_class_list_for_my_grading_spreadsheet/convert_class_list.py
@@ -7,10 +7,10 @@ import sys
 
 infile = open(sys.argv[1])
 
-print "%s,%s,%s,%s,%s,%s,%s,%s" % ("","","","","","","","Quiz HW or Exam")
-print "%s,%s,%s,%s,%s,%s,%s,%s" % ("","","","","","","","Assignment")
-print "%s,%s,%s,%s,%s,%s,%s,%s" % ("","","","","","","","1/1/2014")
-print "%s,%s,%s,%s,%s,%s,%s,%d" % ("Number","Email","Last name","First name","Program","Major","Year",10)
+print("%s,%s,%s,%s,%s,%s,%s,%s" % ("","","","","","","","Quiz HW or Exam"))
+print("%s,%s,%s,%s,%s,%s,%s,%s" % ("","","","","","","","Assignment"))
+print("%s,%s,%s,%s,%s,%s,%s,%s" % ("","","","","","","","1/1/2014"))
+print("%s,%s,%s,%s,%s,%s,%s,%d" % ("Number","Email","Last name","First name","Program","Major","Year",10))
 
 count = 0
 for line in infile:
@@ -24,5 +24,5 @@ for line in infile:
         major = vals[12]
         email = vals[14]
 
-        print "%d,%s,%s,%s,%s,%s,%d,%s" % (count,email,lname,fname,program,major,int(year)," ")
+        print("%d,%s,%s,%s,%s,%s,%d,%s" % (count,email,lname,fname,program,major,int(year)," "))
         count += 1
