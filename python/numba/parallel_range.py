815f76fd1897980cbaba975284d54dabd8b5a37d
mattbellis <matthew.bellis@gmail.com>
2018-02-01 02:59:00 -0500

    Simple numba parallel range example.

--- python/numba/parallel_range.py
+++ python/numba/parallel_range.py
@@ -1,4 +1,8 @@
 from numba import autojit, prange
+import numpy as np
+
+import time
+
 
 @autojit
 def parallel_sum(A):
@@ -8,8 +12,21 @@ def parallel_sum(A):
 
     return sum
 
-import numpy as np
+a = np.random.random((10000,20000))
+print(a.shape[0])
 
-a = np.random.random((100,100))
+t0 = time.time()
+tot = parallel_sum(a)
+#print(tot)
+print(np.sum(tot))
+print("Time to run: %f sec" % (time.time() - t0))
 
-print(parallel_sum(a))
+t0 = time.time()
+#a = a.transpose()
+tot = np.zeros(a.shape[1])
+for i in range(a.shape[1]):
+    #tot[i] = a[:,i].sum()
+    tot[i] = np.sum(a[:,i])
+#print(tot)
+print(np.sum(tot))
+print("Time to run: %f sec" % (time.time() - t0))
