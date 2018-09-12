8b8c6ff91238cd0d42e6fd438f5b87beac1f5e92
mattbellis <matthew.bellis@gmail.com>
2018-09-12 11:49:34 -0400

    Added 2 new variables.

--- bin_scripts/sync_NIU_directory.sh
+++ bin_scripts/sync_NIU_directory.sh
@@ -27,7 +27,7 @@ if ( $1 == "push" ) then
         /home/bellis/eBooks \
         /home/bellis/Work \
         /home/bellis/papers \
-        /home/bellis/latex_stuff \
+        #/home/bellis/latex_stuff \
         #/home/bellis/BaBar \
         /home/bellis/Jobs  \
         /home/bellis/stuff \
@@ -41,9 +41,9 @@ if ( $1 == "push" ) then
             #74.76.135.220:/home/bellis
 
 else if ( $1 == "pull" ) then
-    #rsync -P -r -u -a -v --exclude '*.npy'  --exclude '*JLab*'  --exclude '*.ig' --exclude 'Event_*'   --exclude '*/core' --exclude '*talk*.pdf' --exclude '*lecture*.pdf' --exclude '*chapter*.pdf' --exclude '*/data_skims/*' --exclude '*/processing-2.0b3/*' bellis@192.168.7.23:/home/bellis/{Talks,sketchbook,Work,NIU,BaBar,Jobs,stuff,papers,latex_stuff,bluehost_staging,Stanford,Siena,eBooks,wallpapers,printer_drivers} \
-    rsync -P -r -u -a -v --exclude '*.npy'  --exclude '*JLab*'  --exclude '*.ig' --exclude 'Event_*'   --exclude '*/core' --exclude '*talk*.pdf' --exclude '*lecture*.pdf' --exclude '*chapter*.pdf' --exclude '*/data_skims/*' --exclude '*/processing-2.0b3/*' bellis@mattbellis.dyndns-home.com:/home/bellis/{Talks,sketchbook,Work,NIU,BaBar,Jobs,stuff,papers,latex_stuff,bluehost_staging,Stanford,Siena,eBooks,wallpapers,printer_drivers} \
-#rsync -P -r -u -a -v --exclude '*.npy'  --exclude '*BaBar*'  --exclude '*JLab*'  --exclude '*.ig' --exclude 'Event_*'   --exclude '*/core' --exclude '*talk*.pdf' --exclude '*lecture*.pdf' --exclude '*chapter*.pdf' --exclude '*/data_skims/*' --exclude '*/processing-2.0b3/*' bellis@192.168.7.23:/home/bellis/{Talks,arduino,sketchbook,Work,NIU,BaBar,Jobs,stuff,papers,latex_stuff,bluehost_staging,Stanford,Siena,eBooks,wallpapers,printer_drivers} \
+    #rsync -P -r -u -a -v --exclude '*.npy'  --exclude '*JLab*'  --exclude '*.ig' --exclude 'Event_*'   --exclude '*/core' --exclude '*talk*.pdf' --exclude '*lecture*.pdf' --exclude '*chapter*.pdf' --exclude '*/data_skims/*' --exclude '*/processing-2.0b3/*' bellis@192.168.7.23:/home/bellis/{Talks,sketchbook,Work,NIU,BaBar,Jobs,stuff,papers,bluehost_staging,Stanford,Siena,eBooks,wallpapers,printer_drivers} \
+    rsync -P -r -u -a -v --exclude '*.npy'  --exclude '*JLab*'  --exclude '*.ig' --exclude 'Event_*'   --exclude '*/core' --exclude '*talk*.pdf' --exclude '*lecture*.pdf' --exclude '*chapter*.pdf' --exclude '*/data_skims/*' --exclude '*/processing-2.0b3/*' bellis@mattbellis.dyndns-home.com:/home/bellis/{Talks,sketchbook,Work,NIU,BaBar,Jobs,stuff,papers,bluehost_staging,Stanford,Siena,eBooks,wallpapers,printer_drivers} \
+#rsync -P -r -u -a -v --exclude '*.npy'  --exclude '*BaBar*'  --exclude '*JLab*'  --exclude '*.ig' --exclude 'Event_*'   --exclude '*/core' --exclude '*talk*.pdf' --exclude '*lecture*.pdf' --exclude '*chapter*.pdf' --exclude '*/data_skims/*' --exclude '*/processing-2.0b3/*' bellis@192.168.7.23:/home/bellis/{Talks,arduino,sketchbook,Work,NIU,BaBar,Jobs,stuff,papers,bluehost_staging,Stanford,Siena,eBooks,wallpapers,printer_drivers} \
         /home/bellis
 
 else
--- python/byrnes_video_processing/zip_everything.sh
+++ python/byrnes_video_processing/zip_everything.sh
@@ -1,1 +1,1 @@
-zip -r greg_072418.zip plots variable_files_interpolated plots_max_min_phase files_max_min_phase variable_files_interpolated_summary_stats variable_files
+zip -r greg_091218.zip plots variable_files_interpolated plots_max_min_phase files_max_min_phase variable_files_interpolated_summary_stats variable_files
