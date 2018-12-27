69bfceb5be2e54b056335c5c061c6372cb4cb5e3
mattbellis <matthew.bellis@gmail.com>
2018-12-27 03:27:31 -0500

    Some stuff.

--- python/scikit_learn/kde.py
+++ python/scikit_learn/kde.py
@@ -4,7 +4,8 @@ import matplotlib.pylab as plt
 from sklearn.neighbors.kde import KernelDensity
 
 d0 = np.random.normal(5,0.5,1000)
-d1 = np.random.normal(7,0.3,500)
+#d1 = np.random.normal(7,0.3,500)
+d1 = np.random.lognormal(1,0.25,500) + 5
 
 # The KDE will be expecting an array of arrays, as if it were a multidimensional
 # dataset. 
@@ -14,7 +15,8 @@ data = np.concatenate([d0,d1])[:, np.newaxis]
 plt.figure()
 plt.hist(data,bins=50,density=True)
 
-kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(data)
+kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(data)
+#kde = KernelDensity(kernel='tophat', bandwidth=0.2).fit(data)
 
 # The last bit is to turn it into 100 individual "datasets" of one point each.
 #X_plot = np.linspace(-5, 10, 100)[:, np.newaxis]
