3775005a71d705e17df1f0d713a8bc05c772b369
mattbellis <matthew.bellis@gmail.com>
2019-03-21 17:05:43 -0400

    Added tools to pull tables out of .tex files.

---
+++ python/latex2images/turn_latex_documents_into_images.py
@@ -0,0 +1,42 @@
+import sys
+from TexSoup import TexSoup
+
+import subprocess
+import glob
+import os
+
+
+infile = open(sys.argv[1])
+text = ""
+for line in infile:
+    text += line
+print(text)
+
+soup = TexSoup(text)
+
+tables = list(soup.find_all('table'))
+for count,t in enumerate(tables):
+    output = "\\documentclass{article}\n"
+    output += "\\begin{document}\n"
+    output += "\\pagestyle{empty}\n"
+    output += str(t)
+    output += "\n"
+    output += "\\end{document}\n"
+    print(output)
+
+    outname = "file_image_{0}.png".format(count)
+    if t.caption is not None:
+        outname = (str(t.caption.string)[0:40]).replace(' ','_').replace('.','') + '.png'
+
+    outfile = open("tmp.tex","w")
+    outfile.write(output)
+    outfile.close()
+
+    subprocess.check_call(['pdflatex', 'tmp'])
+    subprocess.check_call(['convert', '-density', '300', 'tmp.pdf', '-flatten', '-trim', '-quality', '100', outname])
+
+    for cruft in glob.glob("tmp.*"):
+          os.remove(cruft)
+
+    
+
