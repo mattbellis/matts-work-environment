7c3e69d29add1173a95d7cfbab7ad5d209ed9fbb
mattbellis <matthew.bellis@gmail.com>
2019-03-25 02:43:30 -0400

    stuff.

--- python/convolution/convolve_function.py
+++ python/convolution/convolve_function.py
@@ -9,7 +9,7 @@ print(x)
 x = np.linspace(0,600,1000)
 y = np.exp(-x/150)
 
-xres = np.linspace(-3,3.0,100)
+xres = np.linspace(-3,3.0,1000)
 res = stats.norm(0,0.01).pdf(xres)
 newy = np.convolve(y,res)#,'same')
 
@@ -19,7 +19,10 @@ print(len(newy),len(y))
 plt.figure()
 plt.subplot(1,2,1)
 plt.plot(x,y)
-plt.plot(newy)
+
+newx = (600/len(newy))*np.linspace(0,len(newy)-1,len(newy))
+print(newx)
+plt.plot(newx,newy/100.0)
 
 
 plt.subplot(1,2,2)
