2d175527d9b1fea8ed45b61ba279cb88a9362e33
Matt Bellis <matthew.bellis@gmail.com>
2020-01-07 16:12:48 -0500

    Stuff.

--- python/Siena/HHMI/analyze.py
+++ python/Siena/HHMI/analyze.py
@@ -93,6 +93,19 @@ courses = np.unique(df['course'].values)
 print(courses)
 
 sns.set(style='whitegrid')
+sns.set()
+#sns.palplot(sns.color_palette("GnBu_d"))
+#sns.set_palette(sns.color_palette("GnBu_d"))
+#sns.set_palette("PuBuGn_d")
+#sns.set_palette("PuBuGn_d")
+#sns.set_palette("husl")
+
+
+#sns.set()
+#sns.palplot(sns.color_palette("Paired"))
+#sns.palplot(sns.color_palette("hls", 8))
+
+
 
 '''
 for course in courses:
@@ -128,17 +141,74 @@ for course in courses:
 
 
 #plt.show()
+# Combine MATH and PHYS
+df['course'][df['course']=='PHYS 110'] = 'PHYS'
+df['course'][df['course']=='PHYS 130'] = 'PHYS'
 
-for course in courses:
+df['course'][df['course']=='MATH 110'] = 'MATH'
+df['course'][df['course']=='MATH 105'] = 'MATH'
+
+df['course'][df['course']=='BIOL 110'] = 'BIOL'
+df['course'][df['course']=='CHEM 110'] = 'CHEM'
+df['course'][df['course']=='CSIS 110'] = 'CSIS'
+
+print(df[df['course']=='PHYS'])
+print(df[df['course']=='MATH'])
+dfmerged = df.groupby(['course','ethnicity','grade']).sum().reset_index()
+print(dfmerged[dfmerged['course']=='PHYS'])
+print(dfmerged[dfmerged['course']=='MATH'])
+#exit()
+
+# Make a data frame out of these
+e = [] # Course
+g = [] # Grade
+s = [] # Fraction
+c = [] # Course
+n = [] # Numbers
+#for course in courses:
+for course in dfmerged['course'].values:
     print("------------------")
     print("------------------")
     print (course)
     print("------------------")
     for i,grade in enumerate(['A','B','C','D','F']):
         print("--- " + grade + " ---")
-        x = df[(df['course']==course) & (df['grade']==grade)]['number']
-        e = df[(df['course']==course) & (df['grade']==grade)]['ethnicity']
-        for a,b in zip(x,e):
-            print("{0:4.1f}% {1:22} (absolute number {2})".format(100*a/x.sum(),b,a))
-        print()
+        x = dfmerged[(dfmerged['course']==course) & (dfmerged['grade']==grade)]['number']
+        eth = dfmerged[(dfmerged['course']==course) & (dfmerged['grade']==grade)]['ethnicity']
 
+        for a,b in zip(x,eth):
+            print("{0:4.1f}% {1:22} (absolute number {2})".format(100*a/x.sum(),b,a))
+            e.append(b)
+            s.append(a/x.sum())
+            c.append(course)
+            g.append(grade)
+            n.append(a)
+        print()
+data = np.array([c,e,g,s,n]).transpose()
+print(data)
+df2 = pd.DataFrame(data,columns=['course','ethnicity','grade','fraction','number'])
+df2['number'] = df2['number'].apply(pd.to_numeric)
+df2['fraction'] = df2['fraction'].apply(pd.to_numeric)
+
+print(df2[(df2['ethnicity']=='White')])
+
+
+sns.set_palette("colorblind")
+#sns.set_color_codes(palette='deep')
+
+for ethnicity in ethnicities:
+    plt.figure(figsize=(14,3))
+    sns.barplot(x="grade", y="fraction", hue='course', data=df2[(df2['ethnicity']==ethnicity) & (df2['course'] != 'ENVA 100')],ci=None, palette='Dark2')
+    #sns.barplot(x="grade", y="fraction", hue='course', data=df2[(df2['ethnicity']==ethnicity)],ci=None, palette='Set2')
+    
+    plt.gcf().suptitle(ethnicity,fontsize=18)
+    plt.ylim(0,0.5)
+    #plt.ylim(0,1.1)
+    plt.xlim(-0.5,5.0)
+    plt.xlabel('')
+    plt.ylabel('Frac. of students receiving grade')
+    plt.tight_layout()
+    plt.savefig(ethnicity.replace('/','_') +'_SUMMARY_ZOOMED.png')
+    #plt.savefig(ethnicity.replace('/','_') +'_SUMMARY.png')
+
+plt.show()
