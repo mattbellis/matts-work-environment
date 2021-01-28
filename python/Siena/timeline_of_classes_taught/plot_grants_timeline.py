2fe8018b855875d951035230aa8f70c9771a60f1
mattbellis <matthew.bellis@gmail.com>
2021-01-28 01:23:23 -0500

    Minor edits

--- python/Siena/timeline_of_classes_taught/plot_grants_timeline.py
+++ python/Siena/timeline_of_classes_taught/plot_grants_timeline.py
@@ -69,8 +69,17 @@ grant = {'institution':'Siena College','funding_agency':'NSF', 'program1': 'EPP'
         'short_description':'ddddddddddddddd'}
 grants.append(grant)
 
+grant = {'institution':'Siena College','funding_agency':'CURCA', 'program1': 'Summer Scholars', 'progron2':None, \
+        'external':False, 'role':'PI', 'coPIs':None, \
+        'name':'CURCA-this', 'title':'A research', \
+        'short_title':'Cloud chamber', \
+        'amount':2000, 'start':dt.datetime(2013,7,1), 'duration':dt.timedelta(days=45), 'funded':True, \
+        'long_description':'ddddddddddddddd', \
+        'short_description':'ddddddddddddddd'}
+grants.append(grant)
 
 print(grants)
+ngrants = len(grants)
 
 #############################################
 # Pandas stuff
@@ -97,11 +106,19 @@ df = pd.DataFrame.from_dict(df_dict)
 
 print(grants)
 
-colors = {'NSF':'blue', 'APS':'red', 'CMS internal':'orange', 'AAPT':'green'}
+colors = {'NSF':'blue', 'APS':'red', 'CMS internal':'orange', 'AAPT':'green', 'CURCA':'yellow'}
+
+################################################################################
+# External
+################################################################################
 
 fig, ax = plt.subplots(figsize=(12,4))
 grant_names = []
 for i,grant in enumerate(grants):
+
+    if grant['external'] is False:
+        continue 
+
     #identifier = grant['identifier'][0]
     #identifier = '{0} - {1}'.format(grant['instances'][0]['name'],grant['identifier'][0])
     identifier = '{0} - {1}'.format(grant['name'],grant['start'].year)
@@ -110,7 +127,7 @@ for i,grant in enumerate(grants):
 
     start,end = grant['start'], grant['start'] + grant['duration']
     xranges = [(start,end-start)]
-    yrange = (i,1.0)
+    yrange = (ngrants - i - 1,1.0)
     # Plot the broken horizontal bars
     print(xranges)
     fc = 'blue'
@@ -124,6 +141,8 @@ for i,grant in enumerate(grants):
 
 #grant_names.reverse()
 print(grant_names)
+grant_names.reverse()
+print(grant_names)
 y_pos = np.arange(0,len(grants),1) + 0.5
 ax.set_yticks(y_pos)
 ax.set_yticklabels(grant_names)
@@ -132,6 +151,61 @@ plt.grid(axis='y')
 ax.xaxis_date()
 plt.tight_layout()
 
+
+################################################################################
+# Internal
+#
+# Stacked bar graph?
+#
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.bar.html
+################################################################################
+
+fig, ax = plt.subplots(figsize=(12,4))
+grant_names = []
+for i,grant in enumerate(grants):
+
+    if grant['external'] is True:
+        continue 
+
+    #identifier = grant['identifier'][0]
+    #identifier = '{0} - {1}'.format(grant['instances'][0]['name'],grant['identifier'][0])
+    identifier = '{0} - {1}'.format(grant['name'],grant['start'].year)
+    grant_names.append(identifier)
+    print(identifier)
+
+    start,end = grant['start'], grant['start'] + grant['duration']
+    xranges = [(start,end-start)]
+    yrange = (ngrants - i - 1,1.0)
+    # Plot the broken horizontal bars
+    print(xranges)
+    fc = 'blue'
+    if grant['funding_agency'] in colors.keys():
+        fc = colors[grant['funding_agency']]
+
+    alpha = 1.0
+    if grant['funded']==False:
+        alpha = 0.2
+    plt.broken_barh(xranges, yrange, facecolors=fc, alpha=alpha)
+
+#grant_names.reverse()
+print(grant_names)
+grant_names.reverse()
+print(grant_names)
+y_pos = np.arange(0,len(grants),1) + 0.5
+ax.set_yticks(y_pos)
+ax.set_yticklabels(grant_names)
+plt.grid(axis='y')
+
+ax.xaxis_date()
+plt.tight_layout()
+
+
+
+
+
+
+plt.show()
+      
 
 
 
