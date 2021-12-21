eee0ba712ee3e5a95b0f1e8e320dc238726f89d7
Matt Bellis <matthew.bellis@gmail.com>
2021-12-21 09:13:48 -0500

    Minor

--- python/Siena_classes/summarizing_class_grades/google_classroom.py
+++ python/Siena_classes/summarizing_class_grades/google_classroom.py
@@ -11,6 +11,7 @@ df = pd.read_csv(infilename)
 colnames = df.columns
 
 hwandquizinfo = {}
+partinfo = {}
 hwinfo = {}
 quizinfo = {}
 hwinfo = {}
@@ -21,9 +22,14 @@ for name in colnames:
     if name.find('QUIZ')>=0:
         quizinfo[name] = []
         hwandquizinfo[name] = []
-    elif name.find('HW')>=0 or name.find('HOMEWORK')>=0:
+    # EDAV
+    elif name.find('HW')>=0 or name.find('HOMEWORK')>=0 or name.find('CLASS')>=0:
+    # Quantum
+    #elif name.find('HW')>=0 or name.find('HOMEWORK')>=0 or name.find('CODING')>=0:
         hwinfo[name] = []
         hwandquizinfo[name] = []
+    elif name.find('PARTICIPATION')>=0 or name.find('PARTICIPATION')>=0:
+        partinfo[name] = []
     elif name.find('MID')>=0:
         midterminfo[name] = []
     elif name.find('FINAL')>=0:
@@ -55,6 +61,11 @@ for key in hwinfo.keys():
     p = float(df[key].iloc[1])
     hwinfo[key] = [d,p]
     #print(key,d,p)
+for key in partinfo.keys():
+    d = df[key].iloc[0]
+    p = float(df[key].iloc[1])
+    partinfo[key] = [d,p]
+    #print(key,d,p)
 for key in midterminfo.keys():
     d = df[key].iloc[0]
     p = float(df[key].iloc[1])
@@ -79,6 +90,9 @@ def summarize(idx,info,df,drop=False):
         d = info[key][0]
         p = info[key][1]
 
+        if d!=d or p!=p:
+            continue
+
         score = float(dftemp[key])
         grade = score/p
         if grade != grade:
@@ -87,7 +101,7 @@ def summarize(idx,info,df,drop=False):
         else:
             grades.append(100*grade)
 
-        if idx==144:
+        if idx==600:
             print(f"{d:12s} max: {p:5.1f}  score: {score:5.1f}  grade: {100*grade:7.2f}    {key}")
     #print(grades)
     if drop==True:
@@ -108,14 +122,19 @@ for i in range(2,len(df)):
 
     hqave = summarize(i,hwandquizinfo,df,True)
     #hqave = summarize(i,hwandquizinfo,df,False)
+    pave = summarize(i,partinfo,df)
     have = summarize(i,hwinfo,df)
     qave = summarize(i,quizinfo,df)
     mave = summarize(i,midterminfo,df)
     fave = summarize(i,finalinfo,df)
 
+    # EDAV 
     ave = 0.5*hqave + 0.25*mave + 0.25*fave
+    print(f"{i:2d} {fname:12s} {lname:18s}   {ave:5.1f}     hw: {hqave:5.1f} m: {mave:5.1f} f: {fave:5.1f}     h: {have:5.1f} q: {qave:5.1f}")
+    # Quantum
+    #ave = 0.25*have + 0.10*pave + 0.30*mave + 0.35*fave
 
-    print(f"{i:2d} {fname:12s} {lname:18s}   {ave:5.1f}     hq: {hqave:5.1f} m: {mave:5.1f} f: {fave:5.1f}     h: {have:5.1f} q: {qave:5.1f}")
+    #print(f"{i:2d} {fname:12s} {lname:18s}   {ave:5.1f}     hw: {have:5.1f} p: {pave:5.1f} m: {mave:5.1f} f: {fave:5.1f}")
 
 
 
