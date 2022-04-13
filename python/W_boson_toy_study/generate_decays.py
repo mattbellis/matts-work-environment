067784ea0aaf90eb40327ccbbcd899ac9f85d816
mattbellis <matthew.bellis@gmail.com>
2022-04-13 16:38:13 -0400

    Some edits

--- python/W_boson_toy_study/generate_decays.py
+++ python/W_boson_toy_study/generate_decays.py
@@ -6,13 +6,23 @@ import ROOT
 import time
 import numpy as np
 
+def smear(V, SMEAR_ENERGY=True, resolutions = [0.0, 0.0, 0.0, 0.0]):
+        E = V.T()
+        if SMEAR_ENERGY:
+            E = np.random.normal(V.T(), np.abs(resolutions[0]*V.T()))
+        px = np.random.normal(V.X(), np.abs(resolutions[1]*V.X()))
+        py = np.random.normal(V.Y(), np.abs(resolutions[1]*V.Y()))
+        pz = np.random.normal(V.Z(), np.abs(resolutions[3]*V.Z()))
+
+        return E,px,py,pz
+
 rnd = None
 if rnd == None:
     rnd = ROOT.TRandom(int(1000000*time.time()))
 
 
 batchMode = False
-nevents = 1000
+nevents = 100000
 
 #masses_Xdecay = array('d', [ 0.10566, 0.10566 ])
 #masses_Xdecay = array('d', [ 5.279, 5.279 ])
@@ -37,7 +47,12 @@ initial_X = ROOT.TLorentzVector();
 
 output = "Ei,pxi,pyi,pzi,"
 output += "E1,px1,py1,pz1,"
-output += "E2,px2,py2,pz2\n"
+output += "E2,px2,py2,pz2,"
+output += "E1smeared,px1smeared,py1smeared,pz1smeared,"
+output += "E2smeared,px2smeared,py2smeared,pz2smeared\n"
+
+resolution_pct = 0.05
+
 print(output)
 for i in range(nevents):
 
@@ -59,6 +74,13 @@ for i in range(nevents):
         output = ""
         output += f"{initial_X.T()},{initial_X.X()},{initial_X.Y()},{initial_X.Z()},"
         output += f"{p_A.T()},{p_A.X()},{p_A.Y()},{p_A.Z()},"
-        output += f"{p_B.T()},{p_B.X()},{p_B.Y()},{p_B.Z()}\n"
+        output += f"{p_B.T()},{p_B.X()},{p_B.Y()},{p_B.Z()},"
+
+        E,px,py,pz = smear(p_A,SMEAR_ENERGY=True,resolutions=[0.01, 0.05, 0.05, 0.05])
+        output += f"{E},{px},{py},{pz},"
+        E,px,py,pz = smear(p_B,SMEAR_ENERGY=True,resolutions=[0.01, 0.05, 0.05, 0.05])
+        output += f"{E},{px},{py},{pz}\n"
+
+
         print(output)
         #print(p_C.T(),p_C.X(),p_C.Y(),p_C.Z())
